fd4113ec497d95610ca3e8535f0f8bb250d38bb3
yoloswagings <BREMME@MacBook-Pro-van-De.local>
2015-10-15 14:18:38 +0200

    Reset

--- Eva_projectg6/app/src/main/java/com/example/bremme/eva_projectg6/LogIn.java
+++ Eva_projectg6/app/src/main/java/com/example/bremme/eva_projectg6/LogIn.java
@@ -2,89 +2,32 @@ package com.example.bremme.eva_projectg6;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.Profile;
-import com.facebook.ProfileTracker;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 public class LogIn extends AppCompatActivity {
 
-    private TextView mTextDetails;
-    private CallbackManager mCallbackManager;
-    private AccessTokenTracker mTokenTracker;
-    private ProfileTracker mProfileTracker;
     private EditText eUsername,ePassword;
     private Button inlogButton;
-    private LoginButton facebookInlogButton;
-    private FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            AccessToken accessToken = loginResult.getAccessToken();
-        }
-
-        @Override
-        public void onCancel() {
-
-        }
-
-        @Override
-        public void onError(FacebookException e) {
-
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        mCallbackManager = CallbackManager.Factory.create();
-
         //verbinden editText en button met layout
         Locale.setDefault(new Locale("nl"));
         eUsername = (EditText) findViewById(R.id.eUsername);
         ePassword = (EditText) findViewById(R.id.ePassword);
         inlogButton = (Button) findViewById(R.id.loginButton);
 
-        facebookInlogButton = (LoginButton) findViewById(R.id.facebookLoginButton);
-        facebookInlogButton.setReadPermissions("user_friends");
-        facebookInlogButton.registerCallback(mCallbackManager, mCallback);
-
-
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        mCallbackManager.onActivityResult(requestCode,resultCode,data);
-    }
-
     public void login(View view)
     {
         //TODO login logica
@@ -95,7 +38,5 @@ public class LogIn extends AppCompatActivity {
         startActivity(i);
         //registreer hyperlink
     }
-
-
 
 }
