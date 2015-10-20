ee249ae2facfc82a8388975cf57701f3c3a48b13
startiik <delanoyedean@gmail.com>
2015-10-20 17:15:58 +0200

    index.html / stijl / script aangemaakt en aangepast

--- Webapp/javascript/app.js
+++ Webapp/javascript/app.js
@@ -2,3 +2,13 @@
  * Created by dean on 20/10/15.
  */
 /** javascript file **/
+
+var app = angular.module('evaApp', []);
+
+app.controller('MainCtrl', [
+	'$scope',
+	function($scope){
+		$scope.test = 'Hello world!';
+	}]);
+
+$('#modal-id').show('toggle');
