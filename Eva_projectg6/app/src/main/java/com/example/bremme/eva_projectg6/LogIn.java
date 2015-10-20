9829c23ad527ca6ffb0e324386a2772ec812e061
brecht tanghe <brecht_tanghe@hotmail.com>
2015-10-20 14:21:16 +0200

    kleine aanpassingen login

--- Eva_projectg6/app/src/main/java/com/example/bremme/eva_projectg6/LogIn.java
+++ Eva_projectg6/app/src/main/java/com/example/bremme/eva_projectg6/LogIn.java
@@ -1,26 +1,50 @@
 package com.example.bremme.eva_projectg6;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.hardware.camera2.params.Face;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Base64;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 public class LogIn extends AppCompatActivity {
 
     private EditText eUsername,ePassword;
     private Button inlogButton;
+    private LoginButton loginButton;
     private UserLocalStore userLocalStore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         //verbinden editText en button met layout
         Locale.setDefault(new Locale("nl"));
         eUsername = (EditText) findViewById(R.id.eUsername);
@@ -28,7 +52,10 @@ public class LogIn extends AppCompatActivity {
         inlogButton = (Button) findViewById(R.id.loginButton);
         userLocalStore = new UserLocalStore(this);
 
+
     }
+
+
     public void login(View view)
     {
 
@@ -41,5 +68,6 @@ public class LogIn extends AppCompatActivity {
         startActivity(i);
         //registreer hyperlink
     }
+
 
 }
