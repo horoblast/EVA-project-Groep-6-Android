f9c2339f0d279f2c73cff70b8f47546ef005d1a5
startiik <delanoyedean@gmail.com>
2015-10-21 21:21:06 +0200

    registreer login pagina in orde gezet

--- Webapp/javascript/app.js
+++ Webapp/javascript/app.js
@@ -5,10 +5,25 @@
 
 var app = angular.module('evaApp', []);
 
-app.controller('MainCtrl', [
-	'$scope',
-	function($scope){
-		$scope.test = 'Hello world!';
-	}]);
+app.controller('MovieController', function($scope, $http) {
+	var pendingTask;
+
+	function fetch() {
+		$http({
+			method: 'POST',
+			url: "http://groep6api.herokuapp.com/register",
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+			transformRequest: function (obj) {
+				var str = [];
+				for (var p in obj)
+					str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+				return str.join("&");
+			},
+			data: {username: "username", password: "password"}
+		}).success(function (result) {
+			console.log(result);
+		});
+	}
+});
 
 $('#modal-id').show('toggle');
